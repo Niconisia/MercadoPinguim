850fcd7a1ae92e84ba4e41ee799ebe523a62b0a9
NicoNicoNii <mariacarlospais@gmail.com>
2021-05-27 22:50:56 -0300

    Mercado

---
+++ src/main/java/Objetos/Fornecedor.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author usuario
+ */
+public class Fornecedor {
+
+    private String represNome;
+    private String endereco;
+    private String razaoSocial;
+    private String Cnpj;
+    private String Telefone;
+
+    /**
+     * @return the represNome
+     */
+    public String getRepresNome() {
+        return represNome;
+    }
+
+    /**
+     * @param represNome the represNome to set
+     */
+    public void setRepresNome(String represNome) {
+        this.represNome = represNome;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the razaoSocial
+     */
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    /**
+     * @param razaoSocial the razaoSocial to set
+     */
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    /**
+     * @return the Cnpj
+     */
+    public String getCnpj() {
+        return Cnpj;
+    }
+
+    /**
+     * @param Cnpj the Cnpj to set
+     */
+    public void setCnpj(String Cnpj) {
+        this.Cnpj = Cnpj;
+    }
+
+    /**
+     * @return the Telefone
+     */
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    /**
+     * @param Telefone the Telefone to set
+     */
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+}
---
+++ src/main/java/Objetos/Produto.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author usuario
+ */
+public class Produto {
+    private String Nome;
+    private String Descricao;
+    private double valUnitario;
+    private int qntProdutos;
+    private String Fornecedor;
+    private double valorTotal;
+
+    /**
+     * @return the Nome
+     */
+    public String getNome() {
+        return Nome;
+    }
+
+    /**
+     * @param Nome the Nome to set
+     */
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    /**
+     * @return the Descricao
+     */
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    /**
+     * @param Descricao the Descricao to set
+     */
+    public void setDescricao(String Descricao) {
+        this.Descricao = Descricao;
+    }
+
+    /**
+     * @return the valUnitario
+     */
+    public double getValUnitario() {
+        return valUnitario;
+    }
+
+    /**
+     * @param valUnitario the valUnitario to set
+     */
+    public void setValUnitario(double valUnitario) {
+        this.valUnitario = valUnitario;
+    }
+
+    /**
+     * @return the qntProdutos
+     */
+    public int getQntProdutos() {
+        return qntProdutos;
+    }
+
+    /**
+     * @param qntProdutos the qntProdutos to set
+     */
+    public void setQntProdutos(int qntProdutos) {
+        this.qntProdutos = qntProdutos;
+    }
+
+    /**
+     * @return the descricao
+     */
+    
+
+    /**
+     * @param descricao the descricao to set
+     */
+   
+
+    /**
+     * @return the valorTotal
+     */
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    /**
+     * @param valorTotal the valorTotal to set
+     */
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    /**
+     * @return the Fornecedor
+     */
+    public String getFornecedor() {
+        return Fornecedor;
+    }
+
+    /**
+     * @param Fornecedor the Fornecedor to set
+     */
+    public void setFornecedor(String Fornecedor) {
+        this.Fornecedor = Fornecedor;
+    }
+    
+}
